4ae7d1244bf7f11ceaee7e7a1b16ab40084c7c34
togacure <togacure@mail.ru>
2017-06-05 14:04:09 +0300

    fix owm rain display

--- src/main/java/ru/meteo/services/OwmMeteoService.java
+++ src/main/java/ru/meteo/services/OwmMeteoService.java
@@ -4,6 +4,8 @@ import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.google.common.base.Objects;
+
 import lombok.val;
 import lombok.extern.slf4j.Slf4j;
 import net.aksingh.owmjapis.OpenWeatherMap;
@@ -25,10 +27,11 @@ public class OwmMeteoService extends AbstractMeteoService {
 		val client = new OpenWeatherMap(properties.getOwmAPPID());
 		try {
 			val currentWeather = client.currentWeatherByCoordinates(latitude.floatValue(), longitude.floatValue());
+			final Float rain = currentWeather.hasRainInstance() ? currentWeather.getRainInstance().getRain() : Float.NaN;
 			val result = new MeteoDataModel(
 					new TemperatureFormatter().formatAsText(fahrenheitToCelsius(currentWeather.getMainInstance().getTemperature())),
 					new HumidityFormatter().formatAsText(currentWeather.getMainInstance().getHumidity()),
-					currentWeather.hasRainInstance() ? new PrecipitationFormatter().formatAsText(currentWeather.getRainInstance().getRain()) : "has no rain");
+					!Objects.equal(rain, Float.NaN) ? new PrecipitationFormatter().formatAsText(rain) : "has no rain");
 			log.info("fetchNewInfo: result: {}", result);
 			return result;
 		} catch (JSONException e) {
